0f706f27fe78cbdec6b44a9060c4bfde1b19d270
aysenV299 <127608265+aysenV299@users.noreply.github.com>
2023-06-25 23:50:17 +0300

    Add files via upload

--- js/slick.js
+++ js/slick.js
@@ -1,6 +1,7 @@
 $('.sections__inner').slick({
     infinite: true,
         speed: 1200,
+        autoplay: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
         responsive: [
